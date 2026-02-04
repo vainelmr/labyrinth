662d44a22f971f793ebc98aa29aff4ad31b7cda7
bussy <97104409+kofiae@users.noreply.github.com>
2026-02-04 13:46:58 +0100

    Add XML documentation to ICrawler interface

--- Labyrinth/Crawl/ICrawler.cs
+++ Labyrinth/Crawl/ICrawler.cs
@@ -2,13 +2,30 @@ using Labyrinth.Items;
 
 namespace Labyrinth.Crawl
 {
+    /// <summary>
+    /// Labyrinth crawler interface.
+    /// </summary>
     public interface ICrawler
     {
+        /// <summary>
+        /// Gets the current X position.
+        /// </summary>
         int X { get; }
+        /// <summary>
+        /// Gets the current Y position.
+        /// </summary>
         int Y { get; }
+        /// <summary>
+        /// Gets the current direction.
+        /// </summary>
         Direction Direction { get; }
-
+        /// <summary>
+        /// Gets the tile in front of the crawler.
+        /// </summary>
         Task<Type> GetFrontTileTypeAsync();
+        /// <summary>
+        /// Tells if the crawler is facing an exit or not
+        /// </summary>
         Task<bool> IsFacingExitAsync();
         Task<MoveResult> TryMoveAsync(Inventory inventory);
     }
