bc3df0a1bf7b234df59e2bc8ec7a12678697d5fb
Bussy <olubusolaogoe@gmail.com>
2026-02-05 09:44:10 +0100

    + tests pour `Crawler` et `Inventory`

--- LabyrinthTest/Crawl/CrawlerRulesMinimalTest.cs
+++ LabyrinthTest/Crawl/CrawlerRulesMinimalTest.cs
@@ -16,7 +16,7 @@ public class CrawlerRulesMinimalTest
         NewLabyrinth(ascii).NewCrawler();
 
     [Test]
-    public async Task Wall_ahead_TryWalk_returns_null_and_position_does_not_change()
+    public async Task Wall_ahead_TryMove_returns_failure_and_position_does_not_change()
     {
         var crawler = NewCrawler("""
             +--+
@@ -27,15 +27,15 @@ public class CrawlerRulesMinimalTest
         var x0 = crawler.X;
         var y0 = crawler.Y;
 
-        var res = await crawler.TryWalk(new MyInventory());
+        var res = await crawler.TryMoveAsync(new MyInventory());
 
-        Assert.That(res, Is.Null);
+        Assert.That(res, Is.InstanceOf<MoveResult.Failure>());
         Assert.That(crawler.X, Is.EqualTo(x0));
         Assert.That(crawler.Y, Is.EqualTo(y0));
     }
 
     [Test]
-    public async Task Room_ahead_TryWalk_returns_inventory_and_position_changes()
+    public async Task Room_ahead_TryMove_returns_success_with_inventory_and_position_changes()
     {
         var crawler = NewCrawler("""
             +/-+
@@ -47,14 +47,16 @@ public class CrawlerRulesMinimalTest
         var x0 = crawler.X;
         var y0 = crawler.Y;
 
-        var res = await crawler.TryWalk(new MyInventory());
+        var res = await crawler.TryMoveAsync(new MyInventory());
 
-        Assert.That(res, Is.Not.Null);
+        Assert.That(res, Is.InstanceOf<MoveResult.Success>());
+        var success = (MoveResult.Success)res;
+        Assert.That(success.TileInventory, Is.Not.Null);
         Assert.That(crawler.X != x0 || crawler.Y != y0, Is.True);
     }
 
     [Test]
-    public async Task Door_ahead_without_key_TryWalk_fails_and_position_does_not_change()
+    public async Task Door_ahead_without_key_TryMove_fails_and_position_does_not_change()
     {
         var crawler = NewCrawler("""
             +--+
@@ -62,25 +64,25 @@ public class CrawlerRulesMinimalTest
             +-/|
             """);
 
-        // se mettre face à la porte (comme dans ton test existant)
+        // se mettre face à la porte
         crawler.Direction.TurnRight(); // face à 'k'
         var emptyBag = new MyInventory();
-        var inv = await crawler.TryWalk(emptyBag); // avance dans la room de clé (mais on NE transfert rien dans le bag)
+        var moveResult = await crawler.TryMoveAsync(emptyBag); // avance dans la room de clé (mais on NE transfert rien dans le bag)
 
         crawler.Direction.TurnRight(); // face à la porte
 
         var x0 = crawler.X;
         var y0 = crawler.Y;
 
-        var res = await crawler.TryWalk(emptyBag);
+        var res = await crawler.TryMoveAsync(emptyBag);
 
-        Assert.That(res, Is.Null);
+        Assert.That(res, Is.InstanceOf<MoveResult.Failure>());
         Assert.That(crawler.X, Is.EqualTo(x0));
         Assert.That(crawler.Y, Is.EqualTo(y0));
     }
 
     [Test]
-    public async Task Door_ahead_with_key_TryWalk_succeeds_and_position_changes()
+    public async Task Door_ahead_with_key_TryMove_succeeds_and_position_changes()
     {
         var lab = NewLabyrinth("""
             +--+
@@ -92,11 +94,14 @@ public class CrawlerRulesMinimalTest
         var bag = new MyInventory();
 
         crawler.Direction.TurnRight(); // face à la room avec la clé
-        var roomInv = await crawler.TryWalk(bag); // rentre dans la room avec la clé
+        var moveResult = await crawler.TryMoveAsync(bag); // rentre dans la room avec la clé
 
         // transférer la clé dans le bag
-        Assert.That(roomInv, Is.Not.Null);
-        await bag.TryMoveItemsFrom(roomInv!, roomInv!.ItemTypes.Select(_ => true).ToList());
+        Assert.That(moveResult, Is.InstanceOf<MoveResult.Success>());
+        var success = (MoveResult.Success)moveResult;
+        Assert.That(success.TileInventory, Is.Not.Null);
+        var itemTypes = await success.TileInventory!.GetItemTypesAsync();
+        await bag.TryMoveItemsFrom(success.TileInventory!, itemTypes.Select(_ => true).ToList());
         Assert.That(bag.HasItems, Is.True);
 
         crawler.Direction.TurnRight(); // face à la porte
@@ -104,9 +109,9 @@ public class CrawlerRulesMinimalTest
         var x0 = crawler.X;
         var y0 = crawler.Y;
 
-        var res = await crawler.TryWalk(bag);
+        var res = await crawler.TryMoveAsync(bag);
 
-        Assert.That(res, Is.Not.Null);
+        Assert.That(res, Is.InstanceOf<MoveResult.Success>());
         Assert.That(crawler.X != x0 || crawler.Y != y0, Is.True);
     }
 }
